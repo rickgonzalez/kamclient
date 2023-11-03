10e112b76fcdaf4b4b49372fa8c3e046364d9ad4
Rick Gonzalez <rick.gonzalez68@gmail.com>
2023-11-03 08:22:00 -0400

    more test setup

--- app/Lobby/page.tsx
+++ app/Lobby/page.tsx
@@ -1,37 +1,78 @@
+"use client"
 
 import Image from 'next/image'
+import * as Colyseus from "colyseus.js";
+import React, { useState } from 'react';
+
 
 export default function Lobby() {
-  
-  
-  
-  
-  
-  
-    return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-       
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By Detomata Formerly{' '}
-            <Image
-              src="/SingLogoB.png"
-              alt="Singularity"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    var [roomId] = useState('');
+    var client = new Colyseus.Client('https://us-atl-3b185468.colyseus.cloud');
     
-    </main>
+   async function createRoom() {
+        try {
+            const room = await client.joinOrCreate("my_room", { name: "Rick"});
+            roomId = room.roomId
+            console.log("joined successfully", roomId);
+            console.log("Object Room ", room);
+          
+          } catch (e) {
+            console.error("join error", e);
+          }
+    }
+    
+
+      async function getRooms() {
+        try {
+            client.getAvailableRooms("my_room").then(rooms => {
+                for (var i=0; i<rooms.length; i++) {
+                    console.log("living room", rooms[i].roomId);
+                }
+              });
+          
+          } catch (e) {
+            console.error("error listing rooms ", e);
+          } 
+      }
+
+     
+      
+
+
+
+
+    return (
+  
+     <main className='flex items-center justify-center min-h-screen from-black-100 via-teal-300 to-white-500 bg-gradient-to-br'>
+    <div className='w-full max-w-lg px-10 py-8 mx-auto bg-white rounded-lg shadow-xl'>
+        <div className='max-w-md mx-auto space-y-6'>
+
+            <div className="flex flex-col">
+                <h1 className="mb-3 text-3xl font-extrabold text-center">Testing</h1>
+
+               
+                <button onClick={createRoom} className="h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-black-800">Join Room {roomId}</button>
+                
+               
+                <button onClick={getRooms} className="h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-black-800">List Rooms</button>
+
+               
+                <button className="h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-black-800">test</button>
+
+                
+            </div>
+
+        </div>
+    </div>
+</main>
+    
+  
   )
 }
+
+
+
+// <div>
+// <button onClick={createRoom}>Click me!</button>
+// <p>The room is  {roomId}</p>
+// </div>
--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
         <a
           href="Lobby"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
+          target="_self"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
