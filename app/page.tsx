7fef746ea07a831ea2a9f18d7dba3380b2ca6b3e
Rick Gonzalez <rick.gonzalez68@gmail.com>
2023-11-13 17:52:34 -0500

    update blogpage

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
 </a>
 
 <a
-  href="https://www.news.kamioza.com/"
+  href="https://www.kamiozablog.com/"
   className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
   target="_blank"
   rel="noopener noreferrer"
