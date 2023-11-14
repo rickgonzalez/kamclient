533493effc39396d800f315058e3b87df4872b52
Rick Gonzalez <rick.gonzalez68@gmail.com>
2023-11-14 14:29:00 -0500

    installed preline and redux again

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
+'use client;'
+
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    import('preline')
+  }, [])
   return <Component {...pageProps} />
 }
