5cef6366b050dc558e75fd5f43f3f47e50278281
Rick Gonzalez <rick.gonzalez68@gmail.com>
2023-11-14 13:58:02 -0500

    restarting

--- app/Providers/route.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import providerdata from './providerlist';
-
-
-export async function GET(request: Request) {
-  return Response.json({ providerdata })
-}
