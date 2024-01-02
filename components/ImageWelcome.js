7c56405ee0228280ed8bfe0eb38cb4c1fde8df13
Rick Gonzalez <rick.gonzalez68@gmail.com>
2024-01-02 13:42:24 -0500

    updated front page

---
+++ components/ImageWelcome.js
@@ -0,0 +1,77 @@
+
+import * as React from 'react'
+import {Image,Flex,Spacer, Box, Container} from '@chakra-ui/react'
+
+
+
+const imagesList = [
+  {
+    id: 1,
+    src:'https://azariaimages.s3.amazonaws.com/postermod4.png',
+    alt: "Image 4",
+  },  
+  {
+      id: 4,
+      src:'https://azariaimages.s3.amazonaws.com/postermod1.png',
+      alt: "Image 1",
+    },
+    {
+      id: 2,
+      src: 'https://azariaimages.s3.amazonaws.com/postermod2.png',
+      alt: "Image 2",
+    },
+    {
+      id: 3,
+      src:'https://azariaimages.s3.amazonaws.com/postermod3.png',
+      alt: "Image 3",
+    },
+    {
+      id: 5,
+      src:'https://azariaimages.s3.amazonaws.com/postermod5.png',
+      alt: "Image 5",
+    },
+    {
+      id: 6,
+      src:'https://azariaimages.s3.amazonaws.com/postermod6.png',
+      alt: "Image 6",
+    },
+    {
+      id: 7,
+      src:'https://azariaimages.s3.amazonaws.com/postermod7.png',
+      alt: "Image 7",
+    },
+  ];
+
+
+       
+   export const ImageTrain =()=>{
+    
+      const [imageIndex, setImageIndex] = React.useState(0);
+    
+      React.useEffect(() => {
+        setInterval(() => {
+          setImageIndex(prev => (
+            prev === imagesList.length - 1 ? 0 : prev + 1
+          ));
+        },8000);
+      },[])
+    
+     console.log(imageIndex)
+      return (
+        <Box>
+           <Image src={imagesList[imageIndex].src} alt={imagesList[imageIndex].alt} />
+        </Box>
+      );
+    
+    }
+      
+       
+     
+    
+       
+   
+   
+  
+
+
+          
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 
 import Image from 'next/image'
 import KamNavBar from '../components/Navbar';
+import {ImageTrain} from '../components/ImageWelcome';
 
 
 
@@ -10,29 +11,12 @@ export default function Home() {
   <KamNavBar currentPage = "/"></KamNavBar>
   <div className="h-[90%]">
     <div className="ContentArea relative h-full">
+      
       <div className="h-full">
-        <div className="flex h-full w-screen items-end justify-end">
-          <Image alt="Azaria Image" width={400} height={900} src="https://azariaimages.s3.amazonaws.com/azpostclrs2.png" className="absolute top-0 h-[90%] w-screen object-contain object-top sm:h-full sm:w-screen-1/2" />
-        </div>
-        <div className="absolute bottom-0 flex">
-        </div>
+       <ImageTrain></ImageTrain>
       </div>
-      <div className="absolute bottom-4 right-0 z-10 flex sm:bottom-0 sm:right-4">
-        <div className="mr-1 flex h-[50px] w-[50px] items-center justify-center border bg-white sm:m-2 sm:h-[80px] sm:w-[80px]">
-          <a href="">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-            </svg>
-          </a>
-        </div>
-        <div className="mr-1 flex h-[50px] w-[50px] items-center justify-center border bg-white sm:m-2 sm:h-[80px] sm:w-[80px]">
-          <a href="">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-            </svg>
-          </a>
-        </div>
-      </div>
+
+     
     </div>
   </div>
 </section>
