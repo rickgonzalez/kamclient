36137c7e855305033b3b9ad3b3863244549a27ac
Rick Gonzalez <rick.gonzalez68@gmail.com>
2024-01-03 20:21:41 -0500

    fixes to the home page

--- components/KamFooter.js
+++ components/KamFooter.js
@@ -1,6 +1,6 @@
 
 import * as React from 'react'
-import {Image,Flex,Spacer, Box, Container, Text} from '@chakra-ui/react'
+import {Image,Flex,Spacer, Box, Container, Text, Center, Stack, Heading, HStack} from '@chakra-ui/react'
 
 
 
@@ -10,12 +10,51 @@ import {Image,Flex,Spacer, Box, Container, Text} from '@chakra-ui/react'
     
     
       return (
-        <Box h='200px' flex='1' bg='black'>
-        <Text color={'white'}>Footer</Text>
-      </Box>
+        <Box  bg={'#0a0909'} h={['150','300','600']} >
+         <Center flex={1} flexDirection={'row'} bg={'black'} h={40}>
+                
+                <Stack spacing='24px'>
+                      <Center flexDirection={'row'}>
+                       
+                        <Image w={['12','40','60']} m={['2','3','5']} src={'https://azariaimages.s3.amazonaws.com/SINGLOGO.png'}></Image>
+                        
+                        <Image w={['12','40','60']} m={['2','3','5']} src={'https://azariaimages.s3.amazonaws.com/fatmanlogo.png'}></Image>
+
+                        <Image w={['12','40','60']} m={['2','3','5']} src={'https://azariaimages.s3.amazonaws.com/ColyseusBWLogo.png'}></Image>
+                      </Center>
+                </Stack>
+            
+          </Center>
+          
+         
+         
+         <Center flexDirection={'row'} bg={'black'}>
+             
+            
+
+            <Flex flex={1} m={2} p={20} flexDirection={'column'} bg={'black'} h={['40','100','300']}></Flex>
+            <Flex flex={1} m={2} p={20} flexDirection={'column'} bg={'black'} h={['40','100','300']}></Flex>
+            
+
+          </Center> 
+          
+          <Center flex={1}  flexDirection={'row'} bg={'black'} h={40}>
+                <Stack mt='6' spacing='3'>
+                  <Heading size='md' color={'white'}>Copyright Detomata 2024</Heading>
+                  <Center>
+                    <Text color={'white'}>
+                      All rights reserved
+                    </Text>
+                  </Center>
+                </Stack>
+          </Center>
+        </Box>
       );
     
     }
       
        
-     
+  //   <Box mx={16} my={10} py={2} flex={1} flexDirection={'column'} bg='#444444' h='5'>
+  //   </Box> 
+ 
+   
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@
 import KamNavBar from '../components/Navbar';
 import {ImageTrain} from '../components/ImageWelcome';
 import{Footer} from '../components/KamFooter'
-import { Flex, Square, Text, Center, Box,Spacer, Image } from '@chakra-ui/react'
+import { Flex, Square, Text, Center, Box,Spacer, Image, Heading, ListItem, UnorderedList, Button } from '@chakra-ui/react'
 
 
 
@@ -18,51 +18,68 @@ export default function Home() {
 
 
 <Flex flexDirection={{base: 'column', md: 'column', lg:'row'  }}>
-  <Box flex='3' bg='black'>
+  <Center flex='2' bg='black'>
       <div className="h-full">
-      <Text  mt={8} ml={5} fontSize='24px' color='white'>CHAPTER 1:</Text>
+      <Text  mt={8} ml={10} fontSize={['12px','16px','24px']} fontWeight='bold' color='white'>CHAPTER 1:</Text>
        <ImageTrain></ImageTrain>
       </div>
-  </Box>
+  </Center>
 
   <Center flex='1' flexDirection={'column'}  bg='black'>
-      <Center p={4} mr={[0,0,30]} h={['100','200','300']} bgImage={{base: '', md: '', lg:"url('https://azariaimages.s3.amazonaws.com/frame1.png')" }}
+      <Center p={4} m={[0,0,8]} h={['100','200','380']} bgImage={{base: '', md: '', lg:"url('https://azariaimages.s3.amazonaws.com/frame1.png')" }}
               bgPosition="center"
               bgRepeat="no-repeat"
               backgroundSize="contain"
-              bg=''
               >
-                 <Text  m={8} fontSize='16px' color='white'>Kamioza is the parent planet of 7 moons 
-                  Chapter 1: Azaria is the beginning  of an epic adventure.
+                <Center flexDirection={'column'}>
+                 <Text  m={8} fontSize={['12px','16px','24px']} color='white'>
                   Come discover lost treasure, restore ancient artifacts and 
-                  unravel mysteries long forgotten.</Text>
+                  unravel mysteries long forgotten. Enter the beautifully bizzare world of Kamioza
+                  and claim your destiny.</Text>
+                <Button colorScheme='whiteAlpha'>Sign Up Now</Button>
+                </Center>
       </Center>
+      
 
-      <Center p={4} mr={[0,0,30]} h={['100','200','300']} bgImage={{base: '', md: '', lg:"url('https://azariaimages.s3.amazonaws.com/frame1.png')" }}
+      <Center p={4} m={[0,0,8]} h={['100','200','380']} bgImage={{base: '', md: '', lg:"url('https://azariaimages.s3.amazonaws.com/frame1.png')" }}
               bgPosition="center"
               bgRepeat="no-repeat"
               backgroundSize="contain"
               >
-                  <Text  m={8} fontSize='16px' color='white'>Gameplay: 
-                    Unique First Person MMORPG
-                    Collaborative puzzle solving
-                    Resource Management 
-                    Spell and item creation system</Text>
+                 
+                 
+                 <Text  m={8} fontSize={['12px','16px','24px']} color='white'>
+                  Recently remastered from its 1997 designs, 
+                  Kamioza brings a simple originality to adventure gaming. It's approachable, mysterious and loads of fun.</Text>
       </Center>
   </Center>
   </Flex>
-  <Flex flexDirection={'row'}>
+
+  <Box  bg={'black'} h={['150','300','800']}>
+   <Flex flexDirection={{base: 'column', md: 'column', lg:'row'  }}>
+                  <UnorderedList fontSize={['12px','16px','24px']} color='white' p={[0,0,14]}>
+                  <Text fontSize={['12px','16px','24px']} fontWeight={'bold'} color='white'>Gameplay:</Text>
+                    <ListItem>Unique First Person MMORPG - Supports dozens of players in a game instance</ListItem>
+                    <ListItem>Collaborative puzzle solving and micro competition</ListItem>
+                    <ListItem>Resource Management of both natural resources and currency</ListItem>
+                    <ListItem>Spell and item creation system - Drop evil spells and bombs on your friends</ListItem>
+                  </UnorderedList>
+   </Flex>
+      
+    
+    <Flex my={20} flexDirection={{base: 'column', md: 'column', lg:'row'  }}>
+        <Center mb={20} py={2} flexDirection={'row'}>
+          <Image src={'https://azariaimages.s3.amazonaws.com/strip.png'}></Image>
+        </Center>
+      </Flex>  
+  </Box>
+
+ 
   <Footer></Footer>
-  </Flex>
 
 
 
 
-
-
-
-
-     
     </div>
   </div>
 </section>
