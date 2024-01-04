ebf74fb93a7ea70452e5a3aeb3dfec3296f0a5a8
Rick Gonzalez <rick.gonzalez68@gmail.com>
2024-01-04 00:56:15 -0500

    updates for navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -57,12 +57,11 @@ export default function KamNavBar(props) {
   }
 
     return (
-        <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
+        <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-gray-800 text-sm py-4 dark:bg-gray-800">
         <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
           <div className="flex items-center justify-between">
             <Link className="inline-flex items-center gap-x-2 text-xl font-semibold dark:text-white" href="#">
-              {/* <img className="w-10 h-auto" src="../docs/Linkssets/img/logo/logo-short.png" alt="Logo"/> */}
-              Kamioza
+              <img className="w-40 h-auto" src="https://azariaimages.s3.amazonaws.com/KamiozaLogo.png" alt="Logo"/> 
             </Link>
             <div className="sm:hidden">
               <button type="button" className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-gray-700 dark:text-white dark:hover:bg-white/10 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" data-hs-collapse="#navbar-image-and-text-2" aria-controls="navbar-image-and-text-2" aria-label="Toggle navigation">
