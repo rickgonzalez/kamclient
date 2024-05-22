dd7287bc417657f1f2fe60e77f07adca546f2e4f
Rick Gonzalez <rick.gonzalez68@gmail.com>
2024-05-21 22:03:57 -0400

    update google info

--- lib/firebase-admin-config.ts
+++ lib/firebase-admin-config.ts
@@ -1,7 +1,16 @@
 import { initializeApp, getApps, cert } from 'firebase-admin/app';
 
+//https://stackoverflow.com/questions/58430162/how-to-load-a-firebase-admin-key-in-node-js
+const myjson = process.env.google_json
+  
+
+
+
 const firebaseAdminConfig = {
-    credential: cert(process.env.NEXT_PUBLIC_FIREBASE_SERVICE_ACCOUNT_KEY_PATH!)
+  //  credential: cert(process.env.NEXT_PUBLIC_FIREBASE_SERVICE_ACCOUNT_KEY_PATH!)
+  credential: cert(
+  JSON.stringify(myjson)
+  )
 }
 
 export function customInitApp() {
